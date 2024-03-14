c82b32104e2ca252bc5a317cffc680b2524b1711
ahmedeldeep28 <eldeep.email@gmail.com>
2024-03-14 23:20:08 +0200

    chore: added types file

---
+++ src/lib/type.tsx
@@ -0,0 +1,9 @@
+export type RoundedType = "none" | "sm" | "md" | "lg" | "full";
+export type ColorType =
+  | "primary"
+  | "secondary"
+  | "warning"
+  | "success"
+  | "danger";
+export type SizeType = "sm" | "md" | "lg";
+export type VariantType = "solid" | "soft" | "outline";
