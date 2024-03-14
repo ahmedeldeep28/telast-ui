c18af4ab6d7c2725e81125965a00f987212328ac
ahmedeldeep28 <eldeep.email@gmail.com>
2024-03-15 00:14:55 +0200

    feat(label): A component added

---
+++ src/components/label/index.tsx
@@ -0,0 +1,1 @@
+export { Label, labelStyles } from "./label";
---
+++ src/components/label/label.tsx
@@ -0,0 +1,36 @@
+import * as React from "react";
+import * as LabelPrimitive from "@radix-ui/react-label";
+import { cva, type VariantProps } from "class-variance-authority";
+
+import { cn } from "@/lib/utils";
+
+const labelStyles = cva(
+  "font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70",
+  {
+    variants: {
+      size: {
+        sm: "text-xs ",
+        md: "text-sm",
+        lg: "text-base",
+      },
+    },
+    defaultVariants: {
+      size: "md",
+    },
+  }
+);
+
+const Label = React.forwardRef<
+  React.ElementRef<typeof LabelPrimitive.Root>,
+  React.ComponentPropsWithoutRef<typeof LabelPrimitive.Root> &
+    VariantProps<typeof labelStyles>
+>(({ className, size, ...props }, ref) => (
+  <LabelPrimitive.Root
+    ref={ref}
+    className={cn(labelStyles({ size }), className)}
+    {...props}
+  />
+));
+Label.displayName = LabelPrimitive.Root.displayName;
+
+export { Label, labelStyles };
