765e824f5300d47e57d8c6ddb9685b0503faec87
ahmedeldeep28 <eldeep.email@gmail.com>
2024-03-15 01:42:00 +0200

    fix("checkbox story"): fix import

--- src/components/checkbox/checkbox.stories.tsx
+++ src/components/checkbox/checkbox.stories.tsx
@@ -1,5 +1,5 @@
 import type { Meta, StoryObj } from "@storybook/react";
-import Checkbox from ".";
+import { Checkbox } from ".";
 
 const meta: Meta<typeof Checkbox> = {
   title: "Component/Checkbox",
