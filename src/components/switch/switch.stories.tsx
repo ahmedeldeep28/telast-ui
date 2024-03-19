d3b3fd000f25d1a599bec5aefaf232b860e7558b
ahmedeldeep28 <eldeep.email@gmail.com>
2024-03-20 01:24:07 +0200

    docs(switch): added this story  Disabled, Checked ,DefaultChecked

--- src/components/switch/switch.stories.tsx
+++ src/components/switch/switch.stories.tsx
@@ -14,5 +14,25 @@ export default meta;
 type Story = StoryObj<typeof meta>;
 
 export const Default: Story = {
-  args: {},
+  args: {
+    color: "primary",
+  },
+};
+
+export const Disabled: Story = {
+  args: {
+    disabled: true,
+  },
+};
+
+export const Checked: Story = {
+  args: {
+    checked: true,
+  },
+};
+
+export const DefaultChecked: Story = {
+  args: {
+    defaultChecked: true,
+  },
 };
