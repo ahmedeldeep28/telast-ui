42faf6930651dd09ea51e1c299a4ae408cb383a1
ahmedeldeep28 <eldeep.email@gmail.com>
2024-03-24 01:58:42 +0200

    feet(CheckboxGroup): add orientation prop

--- src/components/checkbox/checkbox-group.tsx
+++ src/components/checkbox/checkbox-group.tsx
@@ -1,18 +1,33 @@
 import React from "react";
 import { cn } from "@/lib/utils";
+import { cva } from "class-variance-authority";
 
-const CheckboxGroup = React.forwardRef<
-  HTMLDivElement,
-  React.HTMLAttributes<HTMLDivElement>
->(({ className, ...props }, ref) => {
-  return (
-    <div
-      className={cn("flex flex-col gap-2", className)}
-      {...props}
-      ref={ref}
-    ></div>
-  );
+const checkboxGroupStyles = cva("flex gap-2", {
+  variants: {
+    orientation: {
+      vertical: "flex-col",
+      horizontal: "flex-row",
+    },
+  },
+  defaultVariants: {
+    orientation: "vertical",
+  },
 });
+
+interface CheckboxGroupProps extends React.HTMLAttributes<HTMLDivElement> {
+  orientation: "vertical" | "horizontal";
+}
+const CheckboxGroup = React.forwardRef<HTMLDivElement, CheckboxGroupProps>(
+  ({ className, orientation, ...props }, ref) => {
+    return (
+      <div
+        className={cn(checkboxGroupStyles({ orientation }), className)}
+        {...props}
+        ref={ref}
+      ></div>
+    );
+  }
+);
 CheckboxGroup.displayName = "CheckboxGroup";
 
 export { CheckboxGroup };
