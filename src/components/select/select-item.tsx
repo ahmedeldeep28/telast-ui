5f69cfd27cec1bc4186d0626ad68a6718f5ee880
ahmedeldeep28 <eldeep.email@gmail.com>
2024-03-24 21:20:47 +0200

    feet(select): support rtl

--- src/components/select/select-item.tsx
+++ src/components/select/select-item.tsx
@@ -3,13 +3,14 @@ import * as SelectPrimitive from "@radix-ui/react-select";
 import { Check } from "lucide-react";
 
 import { cn } from "@/lib/utils";
+import { cva } from "class-variance-authority";
 
-const selectItemStyle = [
+const selectItemStyle = cva([
   "relative flex w-full cursor-default select-none items-center",
-  "rounded-sm py-1.5 pl-8 pr-2 text-sm outline-none",
+  "rounded-sm py-1.5 ps-2 pe-2 text-sm outline-none",
   "focus:bg-accent focus:text-accent-foreground",
   "data-[disabled]:pointer-events-none data-[disabled]:opacity-50",
-];
+]);
 
 const SelectItem = React.forwardRef<
   React.ElementRef<typeof SelectPrimitive.Item>,
@@ -17,10 +18,10 @@ const SelectItem = React.forwardRef<
 >(({ className, children, ...props }, ref) => (
   <SelectPrimitive.Item
     ref={ref}
-    className={cn(selectItemStyle, className)}
+    className={cn(selectItemStyle(), className)}
     {...props}
   >
-    <span className="absolute left-2 flex h-3.5 w-3.5 items-center justify-center">
+    <span className="absolute end-2 flex h-3.5 w-3.5 items-center justify-center">
       <SelectPrimitive.ItemIndicator>
         <Check className="h-4 w-4" />
       </SelectPrimitive.ItemIndicator>
@@ -31,4 +32,4 @@ const SelectItem = React.forwardRef<
 ));
 SelectItem.displayName = SelectPrimitive.Item.displayName;
 
-export { SelectItem,selectItemStyle };
+export { SelectItem, selectItemStyle };
