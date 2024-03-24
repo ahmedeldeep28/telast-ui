54b7701354a0cc9275eb260d5ea543285689a774
ahmedeldeep28 <eldeep.email@gmail.com>
2024-03-24 21:19:43 +0200

    fix(SelectContent): add class border-input

--- src/components/select/select-content.tsx
+++ src/components/select/select-content.tsx
@@ -6,7 +6,7 @@ import { cva } from "class-variance-authority";
 const selectContentStyles = cva(
   [
     "relative z-50 max-h-96 min-w-[8rem] overflow-hidden",
-    "rounded-md border bg-popover text-popover-foreground shadow-md",
+    "rounded-md border border-input bg-popover text-popover-foreground shadow-md",
     "data-[state=open]:animate-in data-[state=open]:fade-in-0 data-[state=open]:zoom-in-95",
     "data-[state=closed]:animate-out data-[state=closed]:fade-out-0  data-[state=closed]:zoom-out-95",
   ],
