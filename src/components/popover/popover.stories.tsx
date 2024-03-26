fc285af56f17dd0dc6d81ffb845e2f25b11bde72
ahmedeldeep28 <eldeep.email@gmail.com>
2024-03-27 01:56:30 +0200

    fix(HoverCard): fix import avatar

--- src/components/popover/popover.stories.tsx
+++ src/components/popover/popover.stories.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import type { Meta, StoryObj } from "@storybook/react";
 import { Popover, PopoverContent, PopoverTrigger } from ".";
-import Avatar from "../avatar";
+import { Avatar } from "../avatar";
 import { Label } from "../label";
 import { Input } from "../input";
 
