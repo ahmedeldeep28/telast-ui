da8d6d9f185904f5e36901aee8a05d5b75004a16
ahmedeldeep28 <eldeep.email@gmail.com>
2024-04-04 17:52:26 +0200

    fix(Input): remove flex class

--- src/components/input/input.tsx
+++ src/components/input/input.tsx
@@ -6,7 +6,7 @@ import { ColorType, RoundedType, SizeType } from "@/lib/type";
 
 const inputStyles = cva(
   [
-    "flex rounded-md border border-input bg-background placeholder:text-muted-foreground",
+    "rounded-md border border-input bg-background placeholder:text-muted-foreground",
     "h-10 w-full px-3 py-2 text-sm ring-offset-background",
     "focus-visible:outline-none focus-visible:ring-1",
     "file:border-0 file:bg-transparent file:text-sm file:font-medium",
